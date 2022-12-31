61716511ab9df8b931bde0ed6a4113868970a026
Yura Karbushev <yurakarbushev@gmail com>
2022-12-31 13:40:38 +0700

    ad task

---
+++ Task_63/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+Console.Write("Введите N: ");
+var str = Console.ReadLine() ?? "";
+
+int N = int.Parse(str);
+
+string AddN(int n)
+{
+	if (n == 1)
+		return n.ToString();
+	else
+	{
+		return AddN(n - 1) + ", " + n;
+	}
+}
+
+Console.WriteLine(AddN(N));
---
+++ Task_67/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+*/
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+int SumRec(int num, int sum)
+{
+	if (num == 0) return sum;
+	else
+	{
+		sum += num % 10;
+		return SumRec(num / 10, sum);
+	}
+}
+
+Console.WriteLine(SumRec(n, sum));
